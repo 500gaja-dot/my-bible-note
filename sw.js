c9ae96bb7d0bd2757bf26df7ebc2480d993f447a
500gaja-dot <500gaja@gmail.com>
2026-09-08 11:05:00 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='bible-memo-v6-galaxy';
-const APP=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+const CACHE='bible-memo-v8-localdata';
+const APP=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./bible_1961_krv.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
