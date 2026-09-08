7b91afe70f14c64e05449ca75eb741738b540d01
500gaja-dot <500gaja@gmail.com>
2026-09-08 11:15:21 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,13 @@
-const CACHE='bible-memo-v8-localdata';
-const APP=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./bible_1961_krv.json'];
+const CACHE='bible-memo-v9-bookschema';
+const APP=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
   if(e.request.method!=='GET')return;
-  e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(res=>{
-    if(res&&res.status===200){const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});}return res;
-  }).catch(()=>cached)));
+  const u=new URL(e.request.url);
+  if(e.request.mode==='navigate'||u.pathname.endsWith('/index.html')||u.pathname.endsWith('/bible_1961_krv.json')){
+    e.respondWith(fetch(e.request).then(res=>{if(res&&res.status===200){const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});}return res;}).catch(()=>caches.match(e.request)));
+    return;
+  }
+  e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(res=>{if(res&&res.status===200){const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});}return res;})));
 });
